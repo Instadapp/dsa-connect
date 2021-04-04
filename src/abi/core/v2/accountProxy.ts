5c9634c8b1ef4647f68379e9c8c8df603ba056a2
iwan kruger <iwankruger@gmail.com>
2021-04-05 01:33:39 +0200

    ignore web3 typings error

--- src/abi/core/v2/accountProxy.ts
+++ src/abi/core/v2/accountProxy.ts
@@ -1,3 +1,3 @@
 import { AbiItem } from 'web3-utils'
-
+// @ts-ignore
 export const accountProxy: AbiItem[] = [{"inputs":[{"internalType":"address","name":"_implementations","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"implementations","outputs":[{"internalType":"contract AccountImplementations","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}]
