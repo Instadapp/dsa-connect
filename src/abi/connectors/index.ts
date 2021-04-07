d93a791d0d36868cff26c5a42c8c92dcda06dfec
Thrilok Kumar <thrilok2000@gmail.com>
2021-04-07 16:44:30 +0530

    Fixed v2 connectors ABI issue

--- src/abi/connectors/index.ts
+++ src/abi/connectors/index.ts
@@ -2,7 +2,7 @@ export * as connectorsV1 from './v1'
 export * as connectorsV2_M1 from './v2'
 
 import * as connectorsV1 from './v1'
-import * as connectorsV2_M1 from './v2'
+import { connectorsV2_M1 } from './v2'
 
 export type Connector = keyof typeof connectorsV1 | keyof typeof connectorsV2_M1 
 
--- src/abi/connectors/v2/index.ts
+++ src/abi/connectors/v2/index.ts
@@ -1,9 +1,22 @@
-export * from './1INCH-A'
-export * from './AAVE-V1-A'
-export * from './AAVE-V2-A'
-export * from './AUTHORITY-A'
-export * from './BASIC-A'
-export * from './COMP-A'
-export * from './COMPOUND-A'
-export * from './MAKERDAO-A'
-export * from './UNISWAP-A'
+import {ONEINCH_A} from './1INCH-A'
+import {AAVE_V1} from './AAVE-V1-A'
+import {AAVE_V2} from './AAVE-V2-A'
+import {AUTHORITY_A} from './AUTHORITY-A'
+import {BASIC_A} from './BASIC-A'
+import {COMP_A} from './COMP-A'
+import {COMPOUND_A} from './COMPOUND-A'
+import {MAKERDAO_A} from './MAKERDAO-A'
+import {UNISWAP_A} from './UNISWAP-A'
+
+
+export const connectorsV2_M1 =  {
+    "1INCH-A": ONEINCH_A,
+    "AAVE-V1-A": AAVE_V1,
+    "AAVE-V2-A": AAVE_V2,
+    "AUTHORITY-A": AUTHORITY_A,
+    "BASIC-A": BASIC_A,
+    "COMP-A": COMP_A,
+    "COMPOUND-A": COMPOUND_A,
+    "MAKERDAO-A": MAKERDAO_A,
+    "UNISWAP-A": UNISWAP_A
+}
