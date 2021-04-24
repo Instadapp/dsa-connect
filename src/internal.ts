aa8bdbc744746eac27abb559d75c060b501e491b
Thrilok Kumar <thrilok2000@gmail.com>
2021-04-25 01:02:01 +0530

    Added simulation mode

--- src/internal.ts
+++ src/internal.ts
@@ -167,6 +167,12 @@ export class Internal {
    * Returns the input interface required for cast().
    */
   getAddress = async () => {
+    if (this.dsa.config.mode == "node")
+      return this.dsa.web3.eth.accounts.privateKeyToAccount(this.dsa.config.privateKey)
+        .address;
+    else if (this.dsa.config.mode == "simulation") 
+      return this.dsa.config.publicKey
+
     // otherwise, browser
     const addresses = await this.dsa.web3.eth.getAccounts()
 
