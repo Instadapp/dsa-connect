4f1fd5f8a0c78fc17bafe5c0a25aa32fc6f7a2f5
Daksh Miglani <hello@dak.sh>
2021-05-04 00:46:24 +0530

    fix: 🐛 made the required changes to make tests work

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,11 +4,31 @@
 
 import "dotenv/config"
 import "@nomiclabs/hardhat-web3"
+import { task } from 'hardhat/config';
+
+
+task("accounts", "Prints accounts", async (_, { web3, network }) => {
+
+  await network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [process.env.PUBLIC_ADDRESS]
+  }
+  )
+
+  console.log(await web3.eth.getAccounts())
+
+  await web3.eth.sendTransaction({ from: process.env.PUBLIC_ADDRESS, to: process.env.PUBLIC_ADDRESS, value: 1000 })
+
+});
+
 
 const mnemonic = 'west cricket tenant memory offer burger crumble snow solid mushroom wish solid';
 
 module.exports = {
   solidity: "0.7.3",
+  mocha: {
+    timeout: 5 * 20000,
+  },
   networks: {
     hardhat: {
       accounts: {
--- run-test.sh
+++ run-test.sh
@@ -3,9 +3,9 @@
 echo "Running tests for dsa-connect"
 
 echo "Starting ganache-cli"
-npm run ganache:fork &
+npm run hardhat:fork &
 
-sleep 5
+sleep 10
 
 echo "Running tests"
 npm run test
