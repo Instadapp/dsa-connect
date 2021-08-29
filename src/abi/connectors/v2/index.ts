b3f52edac135dca6135dc91e3e8be766c567abb4
Thrilok Kumar <thrilok2000@gmail.com>
2021-08-29 23:06:26 +0530

    Added B-MAKERDAO-A connector

--- src/abi/connectors/v2/index.ts
+++ src/abi/connectors/v2/index.ts
@@ -33,7 +33,9 @@ import {LIQUITY_A} from './LIQUITY-A'
 import {REFLEXER_A} from './REFLEXER-A'
 import {UNISWAP_V3_A} from './UNISWAP-V3-A'
 import {B_COMPOUND_A} from './B-COMPOUND-A'
+import {B_MAKERDAO_A} from './B-MAKERDAO-A'
 export const connectorsV2_M1 =  {
+    "B-MAKERDAO-A": B_MAKERDAO_A,
     "B-COMPOUND-A": B_COMPOUND_A,
     "UNISWAP-V3-A": UNISWAP_V3_A,
     "REFLEXER-A": REFLEXER_A,
