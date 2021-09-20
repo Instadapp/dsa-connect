87a18f501c08a007cd636874fcf52deec1af617b
Ishan Jain <contact@ishanjain.me>
2021-09-21 01:33:02 +0530

    Add support for Arbitrum One

---
+++ src/addresses/arbitrum/connectorsV1.ts
@@ -0,0 +1,6 @@
+export const connectorsV1 = {
+    "AUTHORITY-A": "0x6ce3e607c808b4f4c26b7f6adaeb619e49cabb25",
+    "BASIC-A": "0x9926955e0dd681dc303370c52f4ad0a4dd061687",
+    "BASIC-B": "0xa9b99766e6c676cf1975c0d3166f96c0848ff5ad",
+    "BASIC-C": "0x839c2d3ade63df5b0b8f3e57d5e145057ab41556"
+}
--- src/addresses/index.ts
+++ src/addresses/index.ts
@@ -7,24 +7,30 @@ import { connectorsV1 as connectorsV1_Mainnet } from './mainnet/connectorsV1'
 import { connectorsV2_M1 as connectorsV2_M1_Mainnet } from './mainnet/connectorsV2_M1'
 import { connectorsV1 as connectorsV1_Polygon } from './polygon/connectorsV1'
 import { connectorsV2_M1 as connectorsV2_M1_Polygon } from './polygon/connectorsV2_M1'
+import { connectorsV1 as connectorsV1_Arbitrum } from "./arbitrum/connectorsV1"
 
 export const Addresses = {
-    genesis: '0x0000000000000000000000000000000000000000',
-    core,
-    connectors: {
-        chains: {
-            1: {
-                versions: {
-                    1: connectorsV1_Mainnet,
-                    2: connectorsV2_M1_Mainnet 
-                }
-            },
-            137: {
-                versions: {
-                    1: connectorsV1_Polygon,
-                    2: connectorsV2_M1_Polygon
-                }
-            }
+  genesis: '0x0000000000000000000000000000000000000000',
+  core,
+  connectors: {
+    chains: {
+      1: {
+        versions: {
+          1: connectorsV1_Mainnet,
+          2: connectorsV2_M1_Mainnet,
         },
-    }
+      },
+      137: {
+        versions: {
+          1: connectorsV1_Polygon,
+          2: connectorsV2_M1_Polygon,
+        },
+      },
+      42161: {
+        versions: {
+          1: connectorsV1_Arbitrum
+        }
+      },
+    },
+  },
 }
