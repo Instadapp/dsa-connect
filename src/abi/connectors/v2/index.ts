fab873999812a29ad92732db4b9941f992cb9df0
pradyuman-verma <pradyumnverma27@gmail.com>
2021-12-30 21:14:56 +0530

    update

--- src/abi/connectors/v2/index.ts
+++ src/abi/connectors/v2/index.ts
@@ -56,15 +56,14 @@ import { WMATIC_A } from './WMATIC-A'
 import { QUICKSWAP_A } from './QUICKSWAP-A'
 import { ONEINCH_A } from './1INCH-A'
 import { ONEINCH_B } from './1INCH-B'
-import { ONEINCH_V4_A } from './1INCH-V4-A'
 import { ONEINCH_V3_A } from './1INCH-V3-A'
-import {ONEINCH_V3_A } from './1INCH-V3-A '
+import { ONEINCH_V4_A } from './1INCH-V4-A'
+
 export const connectorsV2_M1 = {
-    "1INCH-V3-A ": ONEINCH_V3_A ,
-  '1INCH-V3-A': ONEINCH_V3_A,
-  '1INCH-V4-A': ONEINCH_V4_A,
   '1INCH-A': ONEINCH_A,
   '1INCH-B': ONEINCH_B,
+  '1INCH-V3-A': ONEINCH_V3_A,
+  '1INCH-V4-A': ONEINCH_V4_A,
   'QUICKSWAP-A': QUICKSWAP_A,
   'INSTAPOOL-C': INSTAPOOL_C,
   'UNISWAP-SELL-BETA': UNISWAP_SELL_BETA,
