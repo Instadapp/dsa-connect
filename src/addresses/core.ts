03312f75343ad355d503790ecbbae6b2fc9fb1bd
Thrilok Kumar <thrilok2000@gmail.com>
2022-01-15 00:53:13 +0530

    added addresses

--- src/addresses/core.ts
+++ src/addresses/core.ts
@@ -2,10 +2,12 @@ import { core as mainnetCore } from './mainnet/core'
 import { core as polygonCore } from './polygon/core'
 import { core as arbitrumCore } from './arbitrum/core'
 import { core as avalancheCore } from './avalanche/core'
+import { core as optimismCore } from './optimism/core'
 
 export const core = {
   1: mainnetCore,
   137: polygonCore,
   42161: arbitrumCore,
-  43114: avalancheCore
+  43114: avalancheCore,
+  10: optimismCore
 }
