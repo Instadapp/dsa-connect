966d31443ee74be9b64da8817a260533eab850cc
Shriya Tyagi <shriya243@gmail.com>
2022-05-04 21:34:18 +0400

    Updated - AAVE-V2-A, BASIC-A

--- src/abi/connectors/v2/BASIC-A.ts
+++ src/abi/connectors/v2/BASIC-A.ts
@@ -2,134 +2,196 @@ import { AbiItem } from 'web3-utils'
 
 export const BASIC_A: AbiItem[] = [
   {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'erc20',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'tokenAmt',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'getId',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'setId',
-        type: 'uint256',
-      },
-    ],
-    name: 'LogDeposit',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'erc20',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'tokenAmt',
-        type: 'uint256',
-      },
-      {
-        indexed: true,
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'getId',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'setId',
-        type: 'uint256',
-      },
-    ],
-    name: 'LogWithdraw',
-    type: 'event',
-  },
-  {
-    inputs: [],
-    name: 'connectorID',
-    outputs: [
-      { internalType: 'uint256', name: '_type', type: 'uint256' },
-      { internalType: 'uint256', name: '_id', type: 'uint256' },
-    ],
-    stateMutability: 'pure',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'erc20', type: 'address' },
-      { internalType: 'uint256', name: 'tokenAmt', type: 'uint256' },
-      { internalType: 'uint256', name: 'getId', type: 'uint256' },
-      { internalType: 'uint256', name: 'setId', type: 'uint256' },
-    ],
-    name: 'deposit',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'getEthAddr',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'pure',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'getEventAddr',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'pure',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'getMemoryAddr',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'pure',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'name',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      { internalType: 'address', name: 'erc20', type: 'address' },
-      { internalType: 'uint256', name: 'tokenAmt', type: 'uint256' },
-      { internalType: 'address payable', name: 'to', type: 'address' },
-      { internalType: 'uint256', name: 'getId', type: 'uint256' },
-      { internalType: 'uint256', name: 'setId', type: 'uint256' },
-    ],
-    name: 'withdraw',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-]
+    "anonymous": false,
+    "inputs": [{
+      "indexed": true,
+      "internalType": "address",
+      "name": "erc20",
+      "type": "address"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "tokenAmt",
+      "type": "uint256"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "LogDeposit",
+    "type": "event"
+  }, {
+    "anonymous": false,
+    "inputs": [{
+      "indexed": true,
+      "internalType": "address",
+      "name": "erc20",
+      "type": "address"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "tokenAmt",
+      "type": "uint256"
+    }, {
+      "indexed": true,
+      "internalType": "address",
+      "name": "from",
+      "type": "address"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "LogDepositFrom",
+    "type": "event"
+  }, {
+    "anonymous": false,
+    "inputs": [{
+      "indexed": true,
+      "internalType": "address",
+      "name": "erc20",
+      "type": "address"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "tokenAmt",
+      "type": "uint256"
+    }, {
+      "indexed": true,
+      "internalType": "address",
+      "name": "to",
+      "type": "address"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "LogWithdraw",
+    "type": "event"
+  }, {
+    "inputs": [{
+      "internalType": "address",
+      "name": "token",
+      "type": "address"
+    }, {
+      "internalType": "uint256",
+      "name": "amt",
+      "type": "uint256"
+    }, {
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "deposit",
+    "outputs": [{
+      "internalType": "string",
+      "name": "_eventName",
+      "type": "string"
+    }, {
+      "internalType": "bytes",
+      "name": "_eventParam",
+      "type": "bytes"
+    }],
+    "stateMutability": "payable",
+    "type": "function"
+  }, {
+    "inputs": [{
+      "internalType": "address",
+      "name": "token",
+      "type": "address"
+    }, {
+      "internalType": "uint256",
+      "name": "amt",
+      "type": "uint256"
+    }, {
+      "internalType": "address",
+      "name": "from",
+      "type": "address"
+    }, {
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "depositFrom",
+    "outputs": [{
+      "internalType": "string",
+      "name": "_eventName",
+      "type": "string"
+    }, {
+      "internalType": "bytes",
+      "name": "_eventParam",
+      "type": "bytes"
+    }],
+    "stateMutability": "payable",
+    "type": "function"
+  }, {
+    "inputs": [],
+    "name": "name",
+    "outputs": [{
+      "internalType": "string",
+      "name": "",
+      "type": "string"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "inputs": [{
+      "internalType": "address",
+      "name": "token",
+      "type": "address"
+    }, {
+      "internalType": "uint256",
+      "name": "amt",
+      "type": "uint256"
+    }, {
+      "internalType": "address payable",
+      "name": "to",
+      "type": "address"
+    }, {
+      "internalType": "uint256",
+      "name": "getId",
+      "type": "uint256"
+    }, {
+      "internalType": "uint256",
+      "name": "setId",
+      "type": "uint256"
+    }],
+    "name": "withdraw",
+    "outputs": [{
+      "internalType": "string",
+      "name": "_eventName",
+      "type": "string"
+    }, {
+      "internalType": "bytes",
+      "name": "_eventParam",
+      "type": "bytes"
+    }],
+    "stateMutability": "payable",
+    "type": "function"
+}]
