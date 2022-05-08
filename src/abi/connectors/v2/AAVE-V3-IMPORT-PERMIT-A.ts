572c23b75c9d2429fada36fa709ebaa354f0fb99
Richa-iitr <richa@cs.iitr.ac.in>
2022-05-08 15:44:47 +0530

    updated abi,addresses

--- src/abi/connectors/v2/AAVE-V3-IMPORT-PERMIT-A.ts
+++ src/abi/connectors/v2/AAVE-V3-IMPORT-PERMIT-A.ts
@@ -36,6 +36,41 @@ export const AAVE_V3_IMPORT_PERMIT_A: AbiItem[] = [
     type: 'function',
   },
   {
+    inputs: [
+      { internalType: 'address', name: 'userAccount', type: 'address' },
+      {
+        components: [
+          { internalType: 'address[]', name: 'supplyTokens', type: 'address[]' },
+          { internalType: 'address[]', name: 'borrowTokens', type: 'address[]' },
+          { internalType: 'bool', name: 'convertStable', type: 'bool' },
+          { internalType: 'uint256[]', name: 'flashLoanFees', type: 'uint256[]' },
+        ],
+        internalType: 'struct Helper.ImportInputData',
+        name: 'inputData',
+        type: 'tuple',
+      },
+      {
+        components: [
+          { internalType: 'uint8[]', name: 'v', type: 'uint8[]' },
+          { internalType: 'bytes32[]', name: 'r', type: 'bytes32[]' },
+          { internalType: 'bytes32[]', name: 's', type: 'bytes32[]' },
+          { internalType: 'uint256[]', name: 'expiry', type: 'uint256[]' },
+        ],
+        internalType: 'struct Helper.SignedPermits',
+        name: 'permitData',
+        type: 'tuple',
+      },
+      { internalType: 'bool[]', name: 'enableCollateral', type: 'bool[]' },
+    ],
+    name: 'importAaveWithCollateral',
+    outputs: [
+      { internalType: 'string', name: '_eventName', type: 'string' },
+      { internalType: 'bytes', name: '_eventParam', type: 'bytes' },
+    ],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
     inputs: [],
     name: 'name',
     outputs: [{ internalType: 'string', name: '', type: 'string' }],
