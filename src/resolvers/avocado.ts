c5c077f62b9608dae4e6e7c75497fc9c0ce5abc3
Shriya Tyagi <shriya243@gmail.com>
2023-03-06 20:38:22 +0400

    Update target address in spells for avoConnectors

--- src/resolvers/avocado.ts
+++ src/resolvers/avocado.ts
@@ -4,6 +4,17 @@ import { Spells } from '../spells'
 import { Version } from '../internal'
 import { Addresses } from '../addresses'
 import { Abi } from '../abi'
+import { connector } from '../abi/core/v1/connector'
+import { hasKey } from '../utils/typeHelper'
+
+interface AvoConnectorMapping {
+    [key: string]: string;
+}
+
+const AvoConnectorMapping: AvoConnectorMapping = {
+    "AAVE-V3-IMPORT-A": "0x6F836B972129a525615452b1Bd80a0F4EcF94fEd",
+    "INSTAPOOL-C": "0xb00e715E0752232dB3AC121ed86c6e23422a5675"
+}
 
 export interface AvocadoAction {
     target: string;
@@ -29,12 +40,25 @@ export class Avocado {
         targets = encodeSpellsData.targets
     } else {
         try {
+            // let spellsDSA = spells.data
+
+            // InstaConnectorsV2 Contract
             const contract = new this.dsa.web3.eth.Contract(
                 Abi.core.versions[2].connectors,
                 Addresses.core[chainId].versions[version].connectors
             )
-            const {isOk, _connectors} = await contract.methods.isConnectors(encodeSpellsData.targets).call()
+
+            let {isOk, _connectors} = await contract.methods.isConnectors(encodeSpellsData.targets).call()
             if (!isOk) throw new Error("Connector not found on registry")
+            
+            // If the connector matches the key in AvoConnectorMapping, replace the target address
+            // with the Avocado connector address.
+            _connectors = _connectors.map((a: string, i: number) => {
+                hasKey(AvoConnectorMapping, spells.data[i].connector)
+                    ? AvoConnectorMapping[encodeSpellsData.targets[i]]
+                    : a
+            })
+
             targets = _connectors;
         } catch (err) {
             throw new Error("Error: not able to resolver connectorName")
