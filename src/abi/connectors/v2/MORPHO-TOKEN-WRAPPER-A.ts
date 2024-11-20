f52b37c8482d444381b512c41166ee1f118f418e
Samarendra Gouda <samarendragouda@gmail.com>
2024-11-20 20:46:55 +0530

    feat: add morpho new token wrapper

---
+++ src/abi/connectors/v2/MORPHO-TOKEN-WRAPPER-A.ts
@@ -0,0 +1,27 @@
+import { AbiItem } from 'web3-utils'
+export const MORPHO_TOKEN_WRAPPER_A: AbiItem[] = [
+  { inputs: [], name: 'LessTokensReceived', type: 'error' },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: 'uint256', name: 'newTokensReceived', type: 'uint256' }],
+    name: 'LogConvertToNewMorpho',
+    type: 'event'
+  },
+  {
+    inputs: [],
+    name: 'convertToNewMorpho',
+    outputs: [
+      { internalType: 'string', name: '_eventName', type: 'string' },
+      { internalType: 'bytes', name: '_eventParam', type: 'bytes' }
+    ],
+    stateMutability: 'payable',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
