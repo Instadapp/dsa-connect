35ee28952eb27164946f0063004d794143a8a8ac
Samarendra Gouda <samarendragouda@gmail.com>
2025-09-24 10:46:04 +0530

    feat: init plasma chain

--- src/dsa.ts
+++ src/dsa.ts
@@ -30,8 +30,8 @@ export type DSAConfig =
     mode?: 'browser'
   }
 
-// ChainId 1 = mainnet, ChainId 137 = matic, 42161 = arbitrum, 43114 = avalanche, 10 = optimism, 250 = fantom, 8453 = base
-export type ChainId = 1 | 137 | 42161 | 43114 | 10 | 250 | 8453
+// ChainId 1 = mainnet, ChainId 137 = matic, 42161 = arbitrum, 43114 = avalanche, 10 = optimism, 250 = fantom, 8453 = base, 9745 = plasma
+export type ChainId = 1 | 137 | 42161 | 43114 | 10 | 250 | 8453 | 9745
 
 export interface Instance {
   id: number
