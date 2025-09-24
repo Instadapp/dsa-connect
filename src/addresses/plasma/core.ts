b45dec7c5bffbd7753ac5cf7c1941f19d6f1c44a
Samarendra Gouda <samarendragouda@gmail.com>
2025-09-24 10:43:43 +0530

    feat: init plasma chain

---
+++ src/addresses/plasma/core.ts
@@ -0,0 +1,19 @@
+export const core = {
+    index: '0x9926955e0Dd681Dc303370C52f4Ad0a4dd061687',
+    list: '0xA9B99766E6C676Cf1975c0D3166F96C0848fF5ad',
+    read: '0x254A12C367160560BCc2B07c0b0a9cc40EF01f25',
+    versions: {
+      1: {
+        account: '0x839c2D3aDe63DF5b0b8F3E57D5e145057Ab41556',
+        connectors: '0xA7c805e4ad4E7B51d2a1eB442B2014a9B63D3703',
+        events: '0x3254Ce8f5b1c82431B8f21Df01918342215825C2',
+      },
+      2: {
+        accountProxy: '0x39d3d5e7c11D61E072511485878dd84711c19d4A',
+        accountDefault: '0x28846f4051EB05594B3fF9dE76b7B5bf00431155',
+        implementations: "0x0a0a82D2F86b9E46AE60E22FCE4e8b916F858Ddc",
+        account: '0x2240770bEc8cB22ed716F2DCcC4F24980A904bC5', // M1
+        connectors: '0x01fEF4d2B513C9F69E34b2f93Ef707FA9Ff60109', // M1
+      }
+    }
+}
